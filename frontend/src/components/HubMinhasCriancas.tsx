1f5abf4b44cd94c92be9b6e5d3bd1064caa51374
AllanSilvaP <allan.silvap2005@gmail.com>
2025-10-21 16:22:21 -0300

    TOKEN P/1HORA e ajustes de JWT

--- frontend/src/components/HubMinhasCriancas.tsx
+++ frontend/src/components/HubMinhasCriancas.tsx
@@ -112,8 +112,8 @@ export default function HubMinhasCriancas() {
 
             {/* Modal */}
             {showModal && (
-                <div className="fixed inset-0 bg-black/40 backdrop-blur-sm flex justify-content items-center z-50">
-                    <div className="bg-white rounded-2xç shadow-xl p-6 w-[400px] relative">
+                <div className="fixed inset-0 bg-black/40 backdrop-blur-sm flex justify-center items-center z-50">
+                    <div className="bg-white rounded-2xl shadow-xl p-6 w-[400px] relative">
                         <button
                             onClick={() => setShowModal(false)}
                             className="absolute top-3 right-3 text-gray-500 hover:text-gray-700"
--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,7 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { createContext, useState, useContext, type ReactNode} from "react";
 import api from "../api/axios"
-import { useEffect } from "react";
+import { useEffect, useCallback } from "react";
+
+//TEMPO PARA DAR REFRESH (1 HORA)
+const REFRESH_INTERVAL = 55 * 60 * 1000;
 
 type AuthContextType = {
     user: any;
@@ -15,6 +18,31 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 export const AuthProvider = ({children}: {children: ReactNode})  => {
     const [user, setUser] = useState<any>(null);
     const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    const refreshAcessToken = useCallback(async () => {
+        const refreshToken = localStorage.getItem("refresh");
+
+        if(!refreshToken) {
+            console.log("Token de Refresh não encontrado. Fazendo logout.")
+            logout();
+            return;
+        }
+
+        try {
+            console.log("Tentanto atualizar o token...")
+            const response = await api.post('/usuarios/token/refresh', {
+                refresh: refreshToken
+            })
+
+            const { access } = response.data;
+            localStorage.setItem("access", access)
+            console.log("Token atualizado com sucesso.");
+        } catch (error) {
+            console.error("Erro ao atualizar o token. Necessário novo login.", error);
+            alert("Sua sessão expirou. Faça login novamente."); // Alerta que você pediu
+            logout();
+        }
+    }, [])
 
     const login = async (username: string, password: string) => {
         const response = await api.post('/usuarios/login/', {username, password});
@@ -45,6 +73,22 @@ export const AuthProvider = ({children}: {children: ReactNode})  => {
             setIsAuthenticated(true)
         }
     }, [])
+
+    useEffect(() => {
+        let interval: number | null = null;
+
+        if(isAuthenticated) {
+            interval = setInterval(refreshAcessToken, REFRESH_INTERVAL);
+        } else if (interval) {
+            clearInterval(interval)
+        }
+
+        return () => {
+            if(interval) {
+                clearInterval(interval)
+            }
+        }
+    }, [isAuthenticated, refreshAcessToken])
 
     return (
         <AuthContext.Provider value = {{user, login, logout, isAuthenticated}}>
